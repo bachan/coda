761916413e45e22a2bea44b6d9bb2b08da4ddd9d
Alexander Pankov <info@varlog.ru>
2010-09-18 19:53:04 +0400

    test for txml as example of coda tests.

---
+++ tests/txml/test_txml.cpp
@@ -0,0 +1,36 @@
+#include <coda/txml.hpp>
+#include <stdio.h>
+
+struct config : public txml::determination_object
+{
+	struct ROOT : public txml::determination_object
+	{
+		std::vector<std::string> s;
+		void determine(txml::parser* p)
+		{
+			p->determineMember("s", s);
+		}
+	};
+
+	ROOT r;
+	void determine(txml::parser* p)
+	{
+		p->determineMember("r", r);
+	}
+};
+
+int test1()
+{
+	config cfg;
+	cfg.load_from_string("<r><s>s1</s><s>s2</s></r>");
+	if (cfg.r.s.size() != 2) return -1;
+
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	if (test1()) return -1;
+
+	return 0;
+}
