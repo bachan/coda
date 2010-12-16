23cb4294fce59cd06f21fefe702e55550df3d117
Sergey Bochenkov <bachan@j3qq4.org>
2010-12-16 08:02:36 +0300

    coda::synque<typename _T>

---
+++ coda/synque.hpp
@@ -0,0 +1,69 @@
+#ifndef __CODA_SYNQUE_HPP__
+#define __CODA_SYNQUE_HPP__
+
+#include <pthread.h>
+#include <deque>
+
+namespace coda {
+
+template <typename _T>
+class synque
+{
+	pthread_cond_t  c;
+	pthread_mutex_t m;
+	std::deque<_T>  d;
+
+public:
+	synque()
+	{
+		pthread_mutex_init(&m, NULL);
+		pthread_cond_init(&c, NULL);
+	}
+
+	~synque()
+	{
+		pthread_cond_destroy(&c);
+		pthread_mutex_destroy(&m);
+	}
+
+	void add(const _T& e)
+	{
+		pthread_mutex_lock(&m);
+		d.push_front(e);
+		pthread_cond_signal(&c);
+		pthread_mutex_unlock(&m);
+	}
+
+	int get(_T& e, int canwait)
+	{
+		pthread_mutex_lock(&m);
+
+		if (d.empty())
+		{
+			if (0 == canwait)
+			{
+				pthread_mutex_unlock(&m);
+				return -1;
+			}
+
+			pthread_cond_wait(&c, &m);
+
+			if (d.empty())
+			{
+				pthread_mutex_unlock(&m);
+				return -1;
+			}
+		}
+
+		e = d.back();
+		d.pop_back();
+
+		pthread_mutex_unlock(&m);
+
+		return 0;
+	}
+};
+
+} /* namespace coda */
+
+#endif /* __CODA_SYNQUE_HPP__ */
