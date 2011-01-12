1b9b0ef4ef47c7b45a45ec76f20e27d6de9cd171
Sergey Bochenkov <bachan@j3qq4.org>
2011-01-12 14:37:09 +0300

    url.hpp

--- coda/url.hpp
+++ coda/url.hpp
@@ -14,9 +14,11 @@ int coda_get_header_buf(coda_header_t& header, const char* s, size_t len);
 int coda_get_params(coda_params_t& params, const char* s);
 int coda_get_header(coda_header_t& header, const char* s);
 
+size_t coda_urlenc(u_char* dst, const u_char* src, size_t sz_src);
+size_t coda_urldec(u_char* dst, const u_char* src, size_t sz_src);
+
 void coda_url_unescape(const char* s, char* dest, size_t sz);
 void coda_url_escape(const char* s, char* dest, size_t sz);
 void coda_url_preunescape(const char* s, char* dest, size_t sz);
-
 
 #endif /* __CODA_URL_HPP__ */
