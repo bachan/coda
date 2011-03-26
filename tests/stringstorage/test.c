89eede7103efbb85d9f7c2b3df6641937be899f2
Alexander Pankov <pianist@vlg0.varlog.ru>
2011-03-27 00:06:39 +0300

    test string storage

---
+++ tests/stringstorage/test.c
@@ -0,0 +1,25 @@
+#include <coda/stringstorage.h>
+#include <stdio.h>
+
+static void mycb(void *user_data, const char *key, void *value)
+{
+	printf("%s:%u\n", key, *(uint32_t*)value);
+}
+
+int main()
+{
+	string_storage ss;
+	string_storage_init(&ss, sizeof(uint32_t));
+
+	uint32_t* A;
+	A = string_storage_add(&ss, "AAA"); *A = 5;
+	A = string_storage_add(&ss, "AAAaaaaaaa"); *A = 25;
+	A = string_storage_add(&ss, "AAAaaaaabbbb"); *A = 26;
+
+	string_storage_enumerate(&ss, mycb, 0);
+	string_storage_free(&ss);
+
+	return 0;
+}
+
+
