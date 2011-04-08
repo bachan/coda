34c98a37999312e1a1bc16618c5faaaf8713fcf9
Sergey Bochenkov <bachan@j3qq4.org>
2011-04-08 17:22:17 +0400

    socket.h functions

--- coda/socket.h
+++ coda/socket.h
@@ -2,6 +2,9 @@
 #define __CODA_SOCKET_H__
 
 #include <errno.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -87,6 +90,95 @@ in_addr_t coda_inet_addr(const char *data, size_t size)
 	}
 
 	return INADDR_NONE;
+}
+
+static inline
+int coda_listen(const char *host, const char *port, int backlog, int nonblock)
+{
+	int sd, rc;
+	struct sockaddr_in addr;
+
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	if (0 > sd) return -1;
+
+	if (nonblock && 0 > (rc = coda_set_nonblk(sd, nonblock)))
+	{
+		close(sd);
+		return -1;
+	}
+
+	if (0 > (rc = coda_set_sckopt(sd, SOL_SOCKET, SO_REUSEADDR, 1)))
+	{
+		close(sd);
+		return -1;
+	}
+
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr(host);
+	addr.sin_port = htons(strtoul(port, NULL, 10));
+
+	if (0 > (rc = bind(sd, (struct sockaddr *) &addr, sizeof(addr))))
+	{
+		close(sd);
+		return -1;
+	}
+
+	if (0 > (rc = listen(sd, backlog)))
+	{
+		close(sd);
+		return -1;
+	}
+
+	return sd;
+}
+
+static inline
+int coda_accept(int fd, struct in_addr *ip, int nonblock)
+{
+	int sd, rc;
+	struct sockaddr_in addr;
+	socklen_t addrlen = sizeof(addr);
+
+	sd = accept(fd, (struct sockaddr *) &addr, &addrlen);
+	if (0 > sd) return -1;
+
+	if (0 > (rc = coda_set_nonblk(sd, nonblock)))
+	{
+		close(sd);
+		return -1;
+	}
+
+	*ip = addr.sin_addr;
+
+	return sd;
+}
+
+static inline
+int coda_connect(const char *host, const char *port, int nonblock)
+{
+	int sd, rc;
+	struct sockaddr_in addr;
+
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	if (0 > sd) return -1;
+
+	if (nonblock && 0 > (rc = coda_set_nonblk(sd, nonblock)))
+	{
+		close(sd);
+		return -1;
+	}
+
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr(host);
+	addr.sin_port = htons(strtoul(port, NULL, 10));
+
+	if (0 > (rc = connect(sd, (struct sockaddr *) &addr, sizeof(addr))) && EINPROGRESS != errno)
+	{
+		close(sd);
+		return -1;
+	}
+
+	return sd;
 }
 
 #endif /* __CODA_SOCKET_H__ */
